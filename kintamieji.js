6abf7247c5a8f86213f8814540702f63940e551b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-03 11:04:06 +0200

    kint

--- kintamieji.js
+++ kintamieji.js
@@ -1,2 +1,45 @@
 'use strict';
 console.log('kintamieji js loaded');
+
+// Primityvus kintamieji
+// let arba const - deklaruojam kintamji, rasom TIK pirma karta sukuriant kintamaji
+let age = 48; // number
+let pi = 3.14; // number
+let year = 2023;
+// pakeisti year reiksme (nebedeklaruojam)
+year = 2024;
+
+// String - textas
+let name = 'James';
+let town = 'London';
+let month = 'Lapkritis';
+
+// Boolean kintamasis - tik 2 reiksmes true arba false
+// isUserLoggedIn - camelCase - pagrindinis js
+// IsUserLoggedIn - PascalCase
+// is-user-logged-in - kebab-calse
+// is_user_logged_in - snake_calse
+let isRaining = false;
+let isUserLoggedIn = true;
+let isSpecial = true;
+
+// null ir undefined
+// null - yra kintamasis bet nera reiksmes, naudojamas isvalyti kintamojo reiksme
+// undefined - kintamojo reiksme po deklaracijos, naudojamas suzinoti ar yra nustatyta reiksme
+
+let userName;
+console.log(userName); // undefined
+userName = 'james123';
+console.log(userName); // james123
+// isvalti userName reiksme
+userName = null;
+console.log(userName); // null
+
+// susikurtumes po kitnamaji savo
+// amziui
+// miestui
+// Vardui
+
+// kintamji kiek man bus metu po 5 metu
+
+// atspausdinti reikmes su console.log()
