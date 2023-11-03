91894dd5bdbe26c86e810a3ff96f8667a2ccb2e0
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-03 10:46:59 +0200

    kintam start

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+// ijungti kai kurias pagal nustatymus nutylimas klaidas
+'use strict'; // butu grieztas
 console.log('hello from main.js');
+
+let title = 'title';
+console.log(title);
+title = 50;
